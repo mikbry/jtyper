40f23808f09c941fdee510f88ab093431ac5343a
Mik <mik@opla.ai>
2020-04-27 16:05:20 +0200

    test: add localstorage mockup

---
+++ src/test/localStorageMock.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Mik BRY
+ * mik@mikbry.com
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const localStorageMock = (() => {
+  let store: Record<string, string> = {};
+
+  return {
+    getItem(key: string) {
+      return store[key] || null;
+    },
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
