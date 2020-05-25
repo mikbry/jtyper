ff4df025e2c59bf7c4463c080553bd1d9c5e59ce
Mik <mik@opla.ai>
2020-05-25 18:26:55 +0200

    chore: rollup commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -79,7 +79,23 @@ const plugins = () => [
   string({
     include: ['**/*.fs', '**/*.vs'],
   }),
-  commonjs({ extensions: ['.js', '.jsx'] }),
+  commonjs({
+    include: 'node_modules/**',
+    namedExports: {
+      'node_modules/react/index.js': [
+        'cloneElement',
+        'createContext',
+        'Component',
+        'createElement'
+      ],
+      'node_modules/react-dom/index.js': ['render', 'hydrate'],
+      'node_modules/react-is/index.js': [
+        'isElement',
+        'isValidElementType',
+        'ForwardRef'
+      ]
+    }
+  }),
   production && terser(),
 ];
 
