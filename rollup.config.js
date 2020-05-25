559b229d21b9edd7d930f8d66b552b77da113f56
Mik <mik@opla.ai>
2020-05-25 19:52:09 +0200

    chore: namedExports useMemo

--- rollup.config.js
+++ rollup.config.js
@@ -86,7 +86,8 @@ const plugins = () => [
         'forwardRef',
         'createContext',
         'Component',
-        'createElement'
+        'createElement',
+        'useMemo',
       ],
       'node_modules/react-dom/index.js': ['render', 'hydrate'],
       'node_modules/react-is/index.js': [
