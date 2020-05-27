a40a7c070eb1853837226158700c2111458235f5
Mik <mik@opla.ai>
2020-05-27 19:52:14 +0200

    feat: wip init from a source folder #36

---
+++ src/store/effects/files/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) Mik BRY
+ * mik@mikbry.com
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+// TODO load index.json
