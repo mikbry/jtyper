f5af2241d3f3bdfc463e4d4cda3c53fc3e29c2d1
Mik <mik.bry@gmail.com>
2020-06-04 17:08:16 +0200

    feat: design publication page (#220)

--- src/pages/Publication/Publication.tsx
+++ src/pages/Publication/Publication.tsx
@@ -7,28 +7,31 @@
  */
 
 import React, { FunctionComponent } from 'react';
-// import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Page from '../../components/Page';
 import AppBar from '../../components/AppBar';
 import Container from '../../components/Container';
 import Notebook from '../../components/Notebook';
+import { StateType } from '../../types';
 
 const Wrapper = styled.div`
   display: flex;
   height: 100%;
 `;
 
-const Publication: FunctionComponent = () => (
-  // const { publisher, publicationId } = useParams();
-  <Page>
-    <AppBar />
-    <Wrapper>
-      <Container>
-        <Notebook />
-      </Container>
-    </Wrapper>
-  </Page>
-);
+const Publication: FunctionComponent = () => {
+  const [publisher] = useSelector((state: StateType) => [state.publisher]);
+  return (
+    <Page>
+      <AppBar noDrawer title={publisher.name} />
+      <Wrapper>
+        <Container noDrawer>
+          <Notebook />
+        </Container>
+      </Wrapper>
+    </Page>
+  );
+};
 
 export default Publication;
